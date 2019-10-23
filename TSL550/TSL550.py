f4bd4f3a5cd30899f6a6aa676a633c88ef767240
TommyCox <55894704+TommyCox@users.noreply.github.com>
2019-10-23 13:09:50 -0600

    updated TSL550 with Ben's copy.

--- TSL550/TSL550.py
+++ TSL550/TSL550.py
@@ -2,8 +2,8 @@ from __future__ import division
 import sys
 import time
 import struct
-
 import serial
+
 
 class TSL550:
     # continuous, two-way, external trigger, constant frequency interval
@@ -30,7 +30,12 @@ class TSL550:
     SWEEP_TRIGGER_WAIT = 3
     SWEEP_JUMP = 4
 
-    def __init__(self, address, baudrate=9600, terminator="\r"):
+    MINIMUM_WAVELENGTH = 1500
+    MAXIMUM_WAVELENGTH = 1630
+
+    LASER_PORT = "COM4"  # Change based on specific connection settings.
+
+    def __init__(self, address=LASER_PORT, baudrate=9600, terminator="\r"):
         """
         Connect to the TSL550. Address is the serial port, baudrate
         can be set on the device, terminator is the string the marks
@@ -494,7 +499,7 @@ class TSL550:
         elif val == "Step":
             mode = 3
         else:
-            raise ValueError("Invalid output trigger mode supplied. Choose from None, Stop, Start, and Step.")
+            raise ValueError("Invalide output trigger mode supplied. Choose from None, Stop, Start, and Step.")
         current_state = int(self.write("TM{}".format(mode)))
         if current_state == 1:
             return "Stop"
@@ -540,13 +545,12 @@ class TSL550:
         for nWave in range(int(num_points)):
             while True:
                 try:
-                    in_byte   = self.device.read(4)
-                    current_wavelength = float(struct.unpack(">I",in_byte)[0]) / 1e4
+                    in_byte = self.device.read(4)
+                    current_wavelength = float(struct.unpack(">I", in_byte)[0]) / 1e4
                     break
                 except:
                     print('Failed to read in wavelength data.')
                     pass
-
 
             wavelength_points.append(current_wavelength)
 
@@ -554,7 +558,7 @@ class TSL550:
         self.write("SU")
         return wavelength_points
 
-    def printStatus(self):
+    def print_status(self):
         """
         Query the status of the laser and print its results.
         """
