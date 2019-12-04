3cef5bd4c1cdf882553161da4c15adcd04f7cfec
Andeloth <37753156+Andeloth@users.noreply.github.com>
2019-12-03 17:53:31 -0700

    Added Measurement class, added some documentation to DAQinterface

--- DAQinterface.py
+++ DAQinterface.py
@@ -27,19 +27,43 @@ class NIDAQInterface:
         self.samples_per_channel = None
 
     def add_channel(self, device, channel):
+        """
+        Adds a channel to the NIDAQ Task object.
+        :param device: the device name
+        :param channel: the channel name
+        :return: n/a
+        """
         self.task.ai_channels.add_ai_voltage_chan(device + "/" + channel)
 
     def add_channels(self, channels):
+        """
+        Adds several channels to the NIDAQ Task object.
+        :param channels: a list of full channel names (device name + channel name)
+        :return: n/a
+        """
         for i in range(len(channels)):
             self.task.ai_channels.add_ai_voltage_chan(channels[i])
 
     def update_timing(self, sample_rate, samples_per_chan):
+        """
+        Sets the sample rate and number of samples to read for the Task
+        :param sample_rate: the desired sample rate
+        :param samples_per_chan: the desired number of data points to read
+        :return: n/a
+        """
         self.sample_rate = sample_rate
         self.samples_per_channel = samples_per_chan
         self.task.timing.cfg_samp_clk_timing(sample_rate, samps_per_chan=samples_per_chan)
 
     def initialize(self, channels=None, sample_rate=CARD_TWO_MAX_SAMPLE_RATE,
                    samples_per_chan=DEFAULT_SAMPLES_PER_CHANNEL):
+        """
+        Initialize the Task with the most commonly used parameters and the specified channels
+        :param channels: a list of channels with full channel names
+        :param sample_rate: a
+        :param samples_per_chan:
+        :return:
+        """
         # Channels only added if specified
         if channels is not None:
             self.add_channels(channels)
